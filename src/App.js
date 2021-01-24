31b7129980e2a1c9c23af72ada850040fc982762
JurugoBrian <jurugu.brian@gmail.com>
2021-01-24 16:30:27 +0300

    Added form and filter button components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import React, {useState} from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
-import Todo from "./components/Todo"
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks)
 
   function addTask(name){
-    alert(name);
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
   }
 
   const taskList = tasks.map(task => (
@@ -22,6 +24,9 @@ function App(props) {
     )
   );
 
+const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -31,7 +36,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
